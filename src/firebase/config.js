dd4709645ef9f59f4619d18f6535c2dcad727c65
Ihor Kocherhin <108453761+ihor07@users.noreply.github.com>
2026-03-12 00:39:59 +0100

    Delete Firebase config and initialization
    Removed Firebase configuration and initialization code.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyANxF9Q9VjIt65kuqeo_7HNWSkV7zxps9U",
-  authDomain: "crystalglanz.firebaseapp.com",
-  projectId: "crystalglanz",
-  storageBucket: "crystalglanz.firebasestorage.app",
-  messagingSenderId: "222961474406",
-  appId: "1:222961474406:web:5c15fc469f542b433bc6a7",
-  measurementId: "G-D495MFRHP7",
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export default app;
